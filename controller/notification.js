120f6919b7eb73ff8c99428f66e30febd28717f6
Tosmim <tosmimforidmehtab@gmail.com>
2025-01-25 12:09:19 +0530

    Updated notification mailer

--- controller/notification.js
+++ controller/notification.js
@@ -1,16 +1,23 @@
 import { Notification } from "../model/notification.js";
 import {sendEmail} from "../utils/mailter.js";
+import dayjs from "dayjs";
 
 export const sendNotification = async (req, res) => {
     try {
-        const notifications = await Notification.find({ status: "UNSENT" });
+        const tomorrow = dayjs().add(1, 'day').startOf('day').toDate();
+        const dayAfterTomorrow = dayjs().add(2, 'day').startOf('day').toDate();
+
+        const notifications = await Notification.find({
+            status: "UNSENT",
+            date: { $gte: tomorrow, $lt: dayAfterTomorrow }
+        });
         console.log(`Found ${notifications.length} notifications to send`);
-        for (const notification of notifications) {
-            sendEmail(notification.to, "Product Return", notification.message);
+        await Promise.all(notifications.map(async (notification) => {
+            await sendEmail(notification.to, "Product Return", notification.message);
             notification.status = "SENT";
             await notification.save();
             console.log(`Notification sent to ${notification.to}`);
-        }
+        }));
         console.log("All notifications sent successfully");
         res.status(200).json({ message: "Notification sent successfully" });
     } catch (error) {
